3a80c97bd90d5ab93d0903dbbf0922bb1619b745
BraeuNerd <57776677+BraeuNerd@users.noreply.github.com>
2022-04-04 22:31:23 +0200

    W1D4 Flora

---
+++ Scripts/W1D4_Flora.R
@@ -0,0 +1,40 @@
+# 30 Day Chart Challenge, Week 1: Comparisons, Day 3: Historical
+
+# -----Load Packages----------------------------------
+
+library(tidyverse)
+library(showtext) # for specific fonts; font_add_google
+library(ggtext) # package to interpret HTML with element_markdown to color titles
+
+# -----Load Fonts-------------------------------
+
+font_add_google("Fira Sans Condensed")
+showtext_auto()
+
+
+# -----DataViz----------------------------------------
+
+eq <- function(x){(-x^2)}
+  
+ggplot() + xlim(-10,40) +
+  geom_function(fun=eq, color = "#009c3b", size = 2) +
+  labs(title = "My house<span style='color:#009c3b;'>plants</span> willingness to <span style='color:#009c3b;'>live</span>",
+       caption = "Viz: @braeuNERD",
+       x = "The love I give my plants",
+       y = "My plants  
+       willingness  
+       to live") +
+  theme(plot.background = element_rect(fill = "white", color = "white"),
+        panel.background = element_rect(fill = "white"),
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        legend.position = "none",
+        plot.title = element_markdown(family = "Fira Sans Condensed",color = "#171717", face="bold", size = 25, margin = margin(20,0,10,0)),
+        plot.caption = element_text(family = "Roboto", hjust = 0.95, color = "grey70", size = 11, margin = margin(0,0,20,0)),
+        axis.title.x = element_markdown(family = "Fira Sans Condensed", color = "#009c3b", size = 15),
+        axis.text.x = element_blank(),
+        axis.text.y = element_blank(),
+        axis.ticks = element_blank(),
+        axis.line = element_line(color = "#171717", size = 1.5),
+        axis.title.y = element_markdown(family = "Fira Sans Condensed", color = "#009c3b", angle = 0, vjust=0.5, size = 15))
+
